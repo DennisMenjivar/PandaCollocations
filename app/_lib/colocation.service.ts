ee2d7383469a288aa6c0e7f8c5d201b4f4e6157b
Dennis Menjivar <dnsmenjivar@gmail.com>
2018-07-19 19:45:21 -0600

    Agregue los campos de ContactInformation

--- app/_lib/colocation.service.ts
+++ app/_lib/colocation.service.ts
@@ -8,6 +8,7 @@ import { Login } from '../_models/Login.model';
 import { Contact } from '../_models/Contact.model';
 import { Category } from '../_models/Category.model';
 import { SubCategory } from '../_models/SubCategory.model';
+import { ContactInformation } from '../_models/ContactInformation.model';
 
 @Injectable()
 
@@ -50,6 +51,17 @@ export class ColocationService {
 
     return this._http.post(this.current_api + 'api/colocatechapi/setContact/', body, requestOptions).map((data: Response) => {
       return 1;
+    });
+  }
+
+  getContactInformation(contact: Contact) {
+    var body = JSON.stringify(contact);
+
+    var headerOptions = new Headers({ 'Content-Type': 'application/json' });
+    var requestOptions = new RequestOptions({ method: RequestMethod.Post, headers: headerOptions })
+
+    return this._http.post(this.current_api + 'api/colocatechapi/getContactInformation/', body, requestOptions).map((data: Response) => {
+      return data.json() as ContactInformation;
     });
   }
 
